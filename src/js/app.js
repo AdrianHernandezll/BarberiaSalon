823e6e73a799e74623813e8ce2e3ff4db0bbe740
AdrianHernandezll <danix08@live.com>
2021-05-10 00:15:19 -0300

    primeros cambios,estilos y funciones

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,35 @@ async function mostrarServicios(){
             const db = await resultado.json();
 
             const {servicios} = db;
+
+            //General el HTML
+            servicios.forEach(servicio => {
+                const {id, nombre, precio} = servicio;
+
+                //DOM Scripting
+                //Generar nombre de servicio
+                const nombreServicio = document.createElement("P");
+                nombreServicio.textContent = nombre;
+                nombreServicio.classList.add('nombre-servicio');
+                //Generar el Precio de Servicio
+                const precioServicio = document.createElement('P');
+                precioServicio.textContent = `$ ${precio}`;
+                precioServicio.classList.add('precio-servicio')
+
+                //Generar un contenedor DIV en servicios
+                const servicioDiv = document.createElement('DIV');
+                servicioDiv.classList.add('servicio');
+
+                //Inyectar precio y nombre
+                servicioDiv.appendChild(nombreServicio);
+                servicioDiv.appendChild(precioServicio);
+
+                console.log(servicioDiv)
+
+                //Inyectar en el HTML
+                document.querySelector('#servicios').appendChild(servicioDiv);
+                
+            });
     } catch (error) {
         console.log(error);
     }
